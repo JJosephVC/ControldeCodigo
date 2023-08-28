13ca7a12881aef9abb4747822ad42c57a8add80b
JJosephVC <138827966+JJosephVC@users.noreply.github.com>
2023-08-28 16:53:13 -0600

    Amonoooos

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,4 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
-Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
-Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
