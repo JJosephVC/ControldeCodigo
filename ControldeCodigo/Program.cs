e2c01acd9335a635a16f2c26492e178a90295e22
Joseph Valdivia Chavarría <josephvaldivia1908@gmail.com>
2023-08-28 16:52:43 -0600

    Ahiii

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,2 +1,4 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
+Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
+Console.WriteLine("What the fuck is this, pruebas en GitHub. Una modificación");
